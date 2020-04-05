695f49dabc588a42ee0992b28f7c10a1acad129e
Loïc Saillant <loic.saillant@fabernovel.com>
2020-04-05 17:22:31 +0200

    Create directory when not exist

--- script_scrapping_stethonet.py
+++ script_scrapping_stethonet.py
@@ -3,28 +3,7 @@ import bs4
 import re
 import pandas as pd
 import np
-
-def parse_page(num_page):
-	response = requests.get(get_page(num_page))
-	if response.status_code != 200:
-		return
-	soup = bs4.BeautifulSoup(response.text, 'html.parser')
-	title = get_title(soup)
-	posts = list(soup.find_all("ul"))
-	emails = list()
-	zipCodes = list()
-	for post in posts:
-		email = str(get_email(post.text))
-		zipCode = str(get_zipCode(post.text))
-		if email != "None" and zipCode != "None":
-			emails.append(email)
-			zipCodes.append(zipCode)
-
-	if len(emails) > 0:
-		df = pd.DataFrame({"Emails" : np.array(emails), "Codes postaux" : np.array(zipCodes)})
-		fileName = "results/" + title + ".csv"
-		df.to_csv(fileName, index=False)
-		print("Process page : " + title + " SUCCEED")
+import os
 
 def get_title(soup):
 	return soup.find('h1').next_element
@@ -49,5 +28,37 @@ def get_zipCode(text):
 			if len(zipCode) > 0:
 				return zipCode[0]
 
+def save_to_csv(title, emails, zipCodes):
+    if len(emails) == 0 and len(zipCodes) == 0:
+        return
+
+    folder = "results/"
+    if not(os.path.exists(folder)):
+        os.mkdir(folder)
+
+    df = pd.DataFrame({"Emails" : np.array(emails), "Codes postaux" : np.array(zipCodes)})
+    fileName = "results/" + title + ".csv"
+    df.to_csv(fileName, index=False)
+    print("Process page : " + title + " SUCCEED")
+
+def parse_page(num_page):
+	response = requests.get(get_page(num_page))
+	if response.status_code != 200:
+	    return
+	soup = bs4.BeautifulSoup(response.text, 'html.parser')
+	title = get_title(soup)
+	posts = list(soup.find_all("ul"))
+	emails = list()
+	zipCodes = list()
+	for post in posts:
+	    email = str(get_email(post.text))
+	    zipCode = str(get_zipCode(post.text))
+	    if email != "None" and zipCode != "None":
+	        emails.append(email)
+	        zipCodes.append(zipCode)
+	
+	save_to_csv(title, emails, zipCodes)
+
+# main
 for i in range(0,200):
 	parse_page(i)
